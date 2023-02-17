627d203e0ddcc180ccd4a5bcc337f621d678f5a0
Amiel Carhyl Lapid <108216915+amiel22@users.noreply.github.com>
2023-02-17 12:01:26 +0800

    Add files via upload
    ID Registration

---
+++ Mang Inasal/Reg_ID/Abs.java
@@ -0,0 +1,10 @@
+interface Abs {
+    public void Firstname();
+    public void Lastname();
+    public void Contact();
+    public void Dataofbirth();
+    public void Address();
+    public void Nationality();
+    public void Gender();
+    public void Age();
+}
---
+++ Mang Inasal/Reg_ID/Ask.java
@@ -0,0 +1,24 @@
+class Ask {
+    public void infos(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+        System.out.println("===================================");
+        System.out.println("= THIS IS THE SSS ID REGISTRATION ="); 
+        System.out.println("===================================");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+        System.out.println("Please Type your full Information.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+        System.out.println("Please follow the instructions.");
+        }
+    }
